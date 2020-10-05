e8b131186f4291e5a83ad23a51a0307e0d07d7c1
Kliever <64734174+Kliever@users.noreply.github.com>
2020-10-05 18:19:08 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-$(function(){$(".slider").slick({arrows:false,fade:true,autoplay:3000,dots:true,responsive:[{breakpoint:820,settings:{dots:false}}]});$(".header-btn").on("click",function(){$(".menu").addClass("active")});$(".close-btn").on("click",function(){$(".menu").removeClass("active")})});
+$(function(){
+  $('.slider').slick({
+    arrows: false,
+    fade: true,
+    autoplay: 3000,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 820,
+        settings: {
+          dots: false
+        }
+      }
+    ]
+  });
+
+  $('.header-btn').on('click', function(){
+    $('.menu').addClass('active');
+  });
+
+  $('.close-btn').on('click', function () {
+    $('.menu').removeClass('active');
+  });
+
+  var wow = new WOW(
+    {
+      mobile: false,
+    }
+  );
+  wow.init();
+
+});
